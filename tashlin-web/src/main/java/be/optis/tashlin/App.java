1ce81b2f89a5ed0fb3306cc94633bc618b4fbd31
andyvdh <andy.vandenheuvel@gmail.com>
2012-03-17 14:51:29 +0100

    setup multi module maven project

---
+++ tashlin-web/src/main/java/be/optis/tashlin/App.java
@@ -0,0 +1,13 @@
+package be.optis.tashlin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
