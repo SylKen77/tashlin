c93184416b065820601bd0d13370433c6b17a7c3
andyvdh <andy.vandenheuvel@gmail.com>
2012-03-18 05:48:43 +0100

    setup tiles

---
+++ tashlin-web/src/main/java/be/optis/tashlin/web/JobsController.java
@@ -0,0 +1,15 @@
+package be.optis.tashlin.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class JobsController {
+
+	@RequestMapping(value = "/jobs.htm", method = RequestMethod.GET)
+	public String view() {
+		return ".jobs";
+	}
+	
+}
