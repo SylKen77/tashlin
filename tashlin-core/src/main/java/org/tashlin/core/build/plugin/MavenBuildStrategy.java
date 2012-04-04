2ca8127b8e7c6dedcba4c973cb016bbb490389d2
avdheuve <avdheuve@192.168.1.105>
2012-04-04 19:58:55 +0200

    clean workspace

---
+++ tashlin-core/src/main/java/org/tashlin/core/build/plugin/MavenBuildStrategy.java
@@ -0,0 +1,11 @@
+package org.tashlin.core.build.plugin;
+
+import org.tashlin.core.build.BuildStrategy;
+
+public class MavenBuildStrategy implements BuildStrategy {
+
+	public void execute(Runtime runtime) {
+		
+	}
+
+}
